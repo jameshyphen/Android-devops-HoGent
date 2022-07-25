b858e5652a030d613ff032dba89eb4ac389a73ba
dzhemaptula <dzhem.aptula@gmail.com>
2022-07-25 02:34:10 +0200

    Add User repo and view model

---
+++ app/src/main/java/com/hogent/pandora/data/user/UserRepository.kt
@@ -0,0 +1,11 @@
+package com.hogent.pandora.data.user
+
+import androidx.lifecycle.LiveData
+
+class UserRepository(private val userDao: UserDao) {
+    val readAllData: LiveData<List<User>> = userDao.readAllData()
+
+    suspend fun addUser(user: User){
+        userDao.addUser(user)
+    }
+}
